d91329a87a7d27a015e1ff28fef9df95908fd6af
Hristo <sto.hristo@gmail.com>
2018-11-21 10:55:00 +0200

    simple jsp startup

--- src/main/java/com/webarity/controllers/TestCtrl.java
+++ src/main/java/com/webarity/controllers/TestCtrl.java
@@ -1,21 +1,48 @@
 package com.webarity.controllers;
 
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.ServletContextAware;
 
-@Controller
-@RequestMapping("/thing")
-public class TestCtrl {
+// @Controller
+// @RequestMapping("/thing")
+public class TestCtrl implements HttpRequestHandler, ServletContextAware {
 
-    private String qweee;
+    private String someString;
+    private ServletContext ctx;
 
-    @GetMapping(path="/test", produces="text/plain;charset=UTF-8")
-    @ResponseBody
+    // @GetMapping(path="/test", produces="text/plain;charset=UTF-8")
+    // @ResponseBody
     public String testt() {
-        return qweee;
+        
+        org.springframework.web.HttpRequestHandler rh;
+        org.springframework.web.context.support.HttpRequestHandlerServlet rhs;
+        return someString;
     }
 
-    public void setQweee(String s) { this.qweee = s; }
+    public void setSomeString(String s) {
+        this.someString = s;
+    }
+
+	@Override
+	public void handleRequest(HttpServletRequest rq, HttpServletResponse rs) throws ServletException, IOException {
+        // rs.getWriter().write(testt() + ctx.getContextPath());
+        ctx.getRequestDispatcher(String.format("/WEB-INF/jsp/test.jsp")).forward(rq, rs);
+        // rs.flushBuffer();
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.ctx = servletContext;
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/test.jsp
@@ -0,0 +1,5 @@
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<h1>TEST</h1>
+<div>${pageContext.getServletContext().getContextPath()}</div>
